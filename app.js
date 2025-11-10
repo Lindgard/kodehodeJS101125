2a3aec416e45056af6e987e017f8c2e40979f7c2
ALP <a.b.lindgard@gmail.com>
2025-11-10 09:14:49 +0100

    added code from temporary project to skip the start of the html and css

---
+++ app.js
@@ -0,0 +1,14 @@
+// const card = document.querySelector(".card");
+
+// card.addEventListener("click", (e) => {
+//   const button = e.target.closest("button");
+//   if (!button) return;
+
+//   const buttons = card.querySelectorAll("button");
+//   const isActive = button.classList.contains("activated-btn");
+//   buttons.forEach((b) => b.classList.remove("activated-btn"));
+
+//   if (!isActive) {
+//     button.classList.add("activated-btn");
+//   }
+// });
