138a6b7430c947c813d96c212b08dcde5d10d102
ALP <a.b.lindgard@gmail.com>
2025-11-10 09:53:24 +0100

    fixed script-tag in html, added the start of the js functionality for the project

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-// const card = document.querySelector(".card");
+const card = document.querySelector(".card");
 
-// card.addEventListener("click", (e) => {
-//   const button = e.target.closest("button");
-//   if (!button) return;
+card.addEventListener("click", (e) => {
+  const button = e.target.closest("button");
+  if (!button) return;
 
-//   const buttons = card.querySelectorAll("button");
-//   const isActive = button.classList.contains("activated-btn");
-//   buttons.forEach((b) => b.classList.remove("activated-btn"));
+  const buttons = card.querySelectorAll("button");
+  const isActive = button.classList.contains("activated-btn");
+  buttons.forEach((b) => b.classList.remove("activated-btn"));
 
-//   if (!isActive) {
-//     button.classList.add("activated-btn");
-//   }
-// });
+  if (!isActive) {
+    button.classList.add("activated-btn");
+  }
+});
+
+console.log("Hello world!");
